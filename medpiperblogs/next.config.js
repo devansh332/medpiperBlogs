0b828933c970aad36ab22ed60d8c8431f4750ed1
devanshk2io <devansh@k2io.com>
2022-01-17 17:22:43 +0530

    tailwind css setup || post author slug setup

--- medpiperblogs/next.config.js
+++ medpiperblogs/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["journomed.com", "secure.gravatar.com"],
+  },
+};
---
+++ medpiperblogs/pages/author/[slug].js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Author() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Author
+
